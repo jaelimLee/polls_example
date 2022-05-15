f289c305fac0429ef91df58dcb148e312a734348
jaelimLee <jaelim6285@naver.com>
2022-05-16 00:35:15 +0900

    1

--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=196)
-    pub_date = models.DateField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
